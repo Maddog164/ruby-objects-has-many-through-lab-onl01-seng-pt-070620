6557bb6b8c2469c7aa634bba93da3da90099b50b
Denise Henderson <denise@henderson3.us>
2020-08-08 01:09:17 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, Patient)
+    Appointment.new(date,Patient,self)
+    #binding.pry
+  
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
